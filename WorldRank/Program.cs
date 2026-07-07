1d470aef0a246324e78210ed43ab87239f473b8b
Vasilis Laoudikos <vaslaou@gmail.com>
2026-07-07 13:44:58 +0300

    SpaceShip simulator

--- WorldRank/Program.cs
+++ WorldRank/Program.cs
@@ -1,1 +1,31 @@
-﻿Console.WriteLine("Hello, World!");
+﻿class Coords
+{
+    public Coords()
+        : this(0, 0)
+    { }
+
+    public Coords(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    public int X { get; set; }
+    public int Y { get; set; }
+
+    public override string ToString() => $"({X},{Y})";
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var p1 = new Coords();
+        Console.WriteLine($"Coords #1 at {p1}");
+        // Output: Coords #1 at (0,0)
+
+        var p2 = new Coords(5, 3);
+        Console.WriteLine($"Coords #2 at {p2}");
+    }
+}
+
